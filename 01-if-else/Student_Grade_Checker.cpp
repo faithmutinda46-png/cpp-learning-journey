c1a8b1a9bf5a984415f51a862d11d9caabe56590
Faith <faithmutinda46@gmail.com>
2026-07-31 12:36:26 +0300

    Added input validation for invalid marks

--- 01-if-else/Student_Grade_Checker.cpp
+++ 01-if-else/Student_Grade_Checker.cpp
@@ -13,16 +13,18 @@ int main(){
 	cout << "Please Enter your Marks : ";
 	cin >> marks;
 	
-	if (marks >= 80){
-		cout << name << " ,Your Grade is A.";
+	if (marks < 0 || marks>100 ){
+		cout << name << " , Invalid marks entered.";
+	}else if(marks >= 80){
+		cout << name << " , Your Grade is A.";
 	}else if(marks >= 70){
-		cout << name << " ,Your Grade is B.";		
+		cout << name << " , Your Grade is B.";		
 	}else if(marks >= 60){
-		cout << name << " ,Your Grade is C.";
+		cout << name << " , Your Grade is C.";
 	}else if(marks >= 50){
-		cout << name << " ,Your Grade is D.";
+		cout << name << " , Your Grade is D.";
 	}else{
-		cout << name << " ,Your Grade is F.";
+		cout << name << " , Your Grade is F.";
 	}
 	return 0;
 }
