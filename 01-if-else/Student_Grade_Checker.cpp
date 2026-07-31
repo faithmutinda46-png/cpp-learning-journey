a0441d91dba0ef60f3f6b0a2c376b241a5fe2f60
Faith <faithmutinda46@gmail.com>
2026-07-31 12:07:31 +0300

    Add files via upload

---
+++ 01-if-else/Student_Grade_Checker.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	
+	string name;
+	int marks;
+	
+	cout << "Please Enter your name : ";
+	cin >> name;
+	
+	cout << "Please Enter your Marks : ";
+	cin >> marks;
+	
+	if (marks >= 80){
+		cout << name << " ,Your Grade is A.";
+	}else if(marks >= 70){
+		cout << name << " ,Your Grade is B.";		
+	}else if(marks >= 60){
+		cout << name << " ,Your Grade is C.";
+	}else if(marks >= 50){
+		cout << name << " ,Your Grade is D.";
+	}else{
+		cout << name << " ,Your Grade is F.";
+	}
+	return 0;
+}
