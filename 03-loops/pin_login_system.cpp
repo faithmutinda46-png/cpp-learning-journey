5674a3b0b23c4f44db87e47256c174f7557ea5a5
Faith <faithmutinda46@gmail.com>
2026-08-07 00:09:53 +0300

    Improved PIN verification system
    Added attempt limit, remaining attempts, and account lock feature.

--- 03-loops/pin_login_system.cpp
+++ 03-loops/pin_login_system.cpp
@@ -1,23 +1,35 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
 	
-	int pin;
+	//Declare varriables
+	
+	int pin = 0;
 	int correctPin = 1234;
+	int attempts = 0;
 	
+	//while loops
 	
-	while(pin != correctPin){
+	while(pin != correctPin && attempts < 3){
 		cout << "Enter your pin : "; 
 	    cin >> pin;
+	    attempts++;
+	    
 	    
 	    if(pin != correctPin){
+	    	if(attempts < 3){
 		
-	    cout << "Wrong pin.Try again. \n";
-	}else{
+	    cout << "Wrong pin.Try again. You have " << 3 - attempts << " attempts remaining. \n";
+	}
+}
+}
+	if(pin == correctPin){
 	
 	cout << "Access granted. Welcome to FAITH Bank";
-}
+}else{
+	cout << "Account locked!";
 }
 	return 0;
 }
