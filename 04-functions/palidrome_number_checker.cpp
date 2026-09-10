686d59db5af0d4961c6a386a3260b5324c574212
Faith <faithmutinda46@gmail.com>
2026-09-10 23:26:39 +0300

    ]palidrome_number_checker.cpp

--- 04-functions/palidrome_number_checker.cpp
+++ 04-functions/palidrome_number_checker.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int pali(int number){//reverse,lastDigit
     int reversed = 0;
     int lastDigit = 0;
+    int originalNumber = number;
     
 	while(number != 0){
     lastDigit = number % 10;
@@ -11,20 +12,19 @@ int pali(int number){//reverse,lastDigit
     number = number / 10;
     
 	}
-  if(number == reversed){
+  if(originalNumber == reversed){
   	cout << "Number is palidrome";
   }else{
   	cout << "Number is not palidrome";
   }
-  return pali;
+  return reversed;
 	
 }
 int main(){
 	int number = 0;
 	cout << "Enter a number to check if its palidrome or not : ";
 	cin >> number;
-
+	
 	cout << pali(number);
 	return 0;
-	
 }
