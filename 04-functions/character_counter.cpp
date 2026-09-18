fc5ab4109287c1ef95101880e61b75195b6b8854
Faith <faithmutinda46@gmail.com>
2026-09-18 23:06:14 +0300

    character_counter.cpp

--- 04-functions/character_counter.cpp
+++ 04-functions/character_counter.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 
+//count all characters in a sentence
 int characters(string letters){
 	int counter = 0;
 	
-	
+	//use for loop to loop through each character in the string
 	for(int i = 0; i < letters.length(); i++){
 	counter++;
 }
