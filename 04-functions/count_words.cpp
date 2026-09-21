0cb308e8bbf2dd92eae241438d045a2351d9004a
Faith <faithmutinda46@gmail.com>
2026-09-21 23:09:24 +0300

    count_words.cpp

--- 04-functions/count_words.cpp
+++ 04-functions/count_words.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 using namespace std;
 
+//create a function that we can reuse.
 int words(string sentence){
 	int count = 0;
 	
 	for(int i = 0; i < sentence.length(); i++){
-		
+
+		// Count the spaces between words.
 		if(sentence[i] == ' '){
 			count++;
 		}
 	}
+
+	// Add 1 because the last word doesn't have a space after it.
 	return count+1;
 	
 }
