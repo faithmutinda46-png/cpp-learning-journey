e1250dd0a0851a5cf76759f8968df4ee17d1b4f1
Faith <faithmutinda46@gmail.com>
2026-09-25 22:08:13 +0300

    largest_number.cpp

--- 04-functions/largest_number.cpp
+++ 04-functions/largest_number.cpp
@@ -1,24 +1,27 @@
 #include<iostream>
 using namespace std;
 
+// Create a function to find the largest number in the array.
 int findLargest(int numbers[5]){
 	int largest = 0;
-	
+
+	//create for loop to check and store the largest number
 	for(int i = 0; i < 5; i++){
 	if(numbers[i] > largest){
 		largest = numbers[i];
 		
 	}
 }
-	
+//return the largest number
 return largest;	
 }
 int main(){
 	int numbers [5];
 	
-		
+	//ask the user for their input	
 	cout << "Enter numbers : \n";
-	
+
+// Use a for loop to get 5 numbers from the user and store them in the array.
 	for(int i = 0; i < 5; i++){
 		cin >>numbers[i];
 	}
